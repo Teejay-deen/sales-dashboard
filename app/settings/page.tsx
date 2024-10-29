f0b4754781d8894806c37231de92d226a8698275
Jimoh Tajudeen <jimoh.tajudeen1017@gmail.com>
2024-10-29 15:04:16 +0000

    refator

---
+++ app/settings/page.tsx
@@ -0,0 +1,17 @@
+
+
+import { log } from 'console'
+import React from 'react'
+
+type Props = {}
+
+export default function page({}: Props) {
+
+  console.log("How are you doing");
+  
+  return (
+    <div className=''>
+      
+    </div>
+  )
+}
